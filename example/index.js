3912ba8b3f9fd19cb3abb2a8ab004b147db2cf66
xpeper <xpepermint@gmail.com>
2015-10-18 16:32:00 +0200

    example updated

--- example/index.js
+++ example/index.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const path = require('path');
-
 const Redis = require('ioredis');
 const redis = new Redis();
 
+const paths = [__dirname]; // where to look for jobs
 const qos = require('..');
-const queue = new qos.Queue(redis, 'qos:queue');
-const schedule = new qos.Schedule(redis, 'qos:schedule');
+const queue = new qos.Queue(redis, 'qos:queue', {paths});
+const schedule = new qos.Schedule(redis, 'qos:schedule', {paths});
 
 // starting queue `qos:queue`
 queue.start();
 // starting schedule queue `qos:schedule`
 schedule.start();
 
-let jobPath = path.join(__dirname, 'jobs/Job');
 // delay job for 10s
-schedule.enqueue({queue, at: Date.now()+10000, path: jobPath, args: [Date.now()]});
+schedule.enqueue({queue, at: Date.now()+10000, path: 'jobs/MyJob', args: [Date.now()]});
 // run job now
-queue.enqueue({path: jobPath, args: [Date.now()]});
+queue.enqueue({path: 'jobs/MyJob', args: [Date.now()]});
